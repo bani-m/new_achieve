3b175d0f6c7f7bbc57bfce78077598b577ea54a6
baniianizm <banibanibani50@gmail.com>
2016-08-17 13:35:15 +0000

    first commit

--- Gemfile
+++ Gemfile
@@ -47,4 +47,8 @@ gem 'jquery-turbolinks'
 gem 'rails_12factor', group: :production
  
 gem 'devise'
+
 gem 'faker'
+
+gem 'forgery'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
 100.times do |n|
-  email = Faker::Internet.email
+  email = Faker::Internet.free_email
+  name = Forgery:: Name.full_name
   password = "password"
-  User.create!(email: email,
+  User.create!(name: name,
+               email: email,
                password: password,
                password_confirmation: password,
                )
 end
+100.times do |s|
+  user_id = Faker:: Number.between(1,100)
+  title = Forgery:: Name.full_name  
+  content = Faker:: Team.creature
+  Blog.create!(title: title,
+               content: content,
+               user_id: user_id
+               )
+end               
