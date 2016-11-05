e46eeb40af2c444225bdfd0ba1ed71ce0877aa96
Yasuhiro <banibanibani50@gmail.com>
2016-11-05 14:37:40 +0000

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::Base
    PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
   private
-
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
