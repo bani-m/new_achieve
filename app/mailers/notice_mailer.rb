30379e4060b6446aea1570d90dea0933caf4a81e
Yasuhiro <banibanibani50@gmail.com>
2016-11-12 05:17:38 +0000

    “コミットの内容”

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,11 +11,11 @@ class NoticeMailer < ApplicationMailer
     mail to: "banibanibani50@gmail.com",
          subject: '【Achieve】ブログが投稿されました'
    end
-   
+
    def sendmail_contact(contact)
     @contact = contact
 
-    mail to: "banibanibani50@gmail.com",
+    mail to:  user.email,
          subject: 'お問い合わせが完了しました'
   end
 end
