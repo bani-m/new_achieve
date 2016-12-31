39274e8a3467b06822669e5c773bed6915ed3d11
Yasuhiro <banibanibani50@gmail.com>
2016-12-31 16:23:51 +0000

    jqary comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   # コメントを保存、投稿するためのアクションです。
   def create
-    # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
 
@@ -10,7 +9,6 @@ class CommentsController < ApplicationController
        if @comment.save
          format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
          format.json { render :show, status: :created, location: @comment }
-         # JS形式でレスポンスを返します。
          format.js { render :index }
        else
          format.html { render :new }
@@ -25,8 +23,8 @@ class CommentsController < ApplicationController
     flash[:notice] = "Successfully destroyed comment."
     @comments = Comment.all
    end
+  # ストロングパラメーター
   private
-    # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
     end
