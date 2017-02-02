8f05fc509c8ecf1208f6a6772ddf51309edd82bf
Yasuhiro Ikeba <banibanibani50@gmail.com>
2017-02-02 14:23:50 +0000

    folloe_three

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  
   devise_for :users, controllers: {
   registrations: "users/registrations",
   omniauth_callbacks: "users/omniauth_callbacks"
