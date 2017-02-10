01fb492a8e4cb042184b39c1c9a6917663860b04
Yasuhiro Ikeba <banibanibani50@gmail.com>
2017-02-10 06:27:53 +0000

    dive16_follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,9 @@ class UsersController < ApplicationController
   end
 
 def show
-@user = User.find(params[:id])
+  @user = User.find(params[:id])
 end
 
-def followed_users
-  @users = @user.followed_users
-end
-
-def followers
-  @users = @user.followers
-end
 
 
 end
