9f338c09a9c212be61baa5d22de43def9d6da15c
Yasuhiro Ikeba <banibanibani50@gmail.com>
2017-02-12 03:31:11 +0000

    DIVE19-1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,28 @@
 class CommentsController < ApplicationController
   # コメントを保存、投稿するためのアクションです。
   def create
+    # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
 
-    # respond_toは、クライアントからの要求に応じてレスポンスのフォーマットを変更します。
-     respond_to do |format|
-       if @comment.save
-         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-         format.json { render :show, status: :created, location: @comment }
-         format.js { render :index }
-       else
-         format.html { render :new }
-         format.json { render json: @comment.errors, status: :unprocessable_entity }
-       end
-     end
-   end
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    flash[:notice] = "Successfully destroyed comment."
-    @comments = Comment.all
-   end
-  # ストロングパラメーター
+  @comment = Comment.find(params[:id])
+  @comment.destroy
+  flash[:notice] = "Successfully Deleted"
+end
+
   private
+    # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
+
+
   if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  
+
+  resources :blogs do
+  resources :comments
+  collection do
+    post :confirm
+  end
+  end
+
+  resources :conversations do
+  resources :messages
+  end
+
   devise_for :users, controllers: {
   registrations: "users/registrations",
   omniauth_callbacks: "users/omniauth_callbacks"
