3e1badf943c642aafd1740c74e70ac2bb7cb1241
Yasuhiro Ikeba <banibanibani50@gmail.com>
2017-03-09 16:19:26 +0000

    change storage to fog from file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'new_achieve'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/bani-m/new_achieve.git'
+set :repo_url, 'https://github.com/bani-m/new_achieve'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/new_achieve'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '52.197.127.100', user: 'app', roles: %w{app db web}
+server '172.31.17.35', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/c/Users/yasu0729/.ssh/id_rsa'
