6b5ea84d7b98aff5ebd84b08b79f85c8afc1d2cf
Yasuhiro Ikeba <banibanibani50@gmail.com>
2017-03-22 14:12:30 +0000

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'new_achieve'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/bani-m/new_achieve'
+set :repo_url, 'https://github.com/bani-m/new_achieve.git'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
